c30e25cedab5f2c33ac3c4a08e313e40330a7b74
Lee Wiltbank <lee.wiltbank@hach.com>
2021-06-24 06:07:54 -0700

    AQI-1276 Generate the python bindings source tarball and wheel file

---
+++ nuget/gen_python.ps1
@@ -0,0 +1,25 @@
+﻿
+mkdir -Force ./claros_interfaces_python
+
+Copy-Item ./python/* ./claros_interfaces_python
+mkdir -Force ././claros_interfaces_python/claros_interfaces
+Copy-Item ./claros.interfaces.protocolbuffers/proto/flat/out/python/*.* ./claros_interfaces_python/claros_interfaces
+Move-Item -Force ./claros_interfaces_python/__init__.py ./claros_interfaces_python/claros_interfaces
+Copy-Item -Force ./claros_interfaces_python/claros_interfaces.pth ./claros_interfaces_python/claros_interfaces
+
+Set-Location ./claros_interfaces_python
+
+# Make a source package
+$Command = "python setup.py clean build sdist"
+Write-Output `n$Command
+Invoke-Expression -Command $Command
+
+# Make a binary package
+$Command = "python setup.py clean build bdist_wheel"
+Write-Output `n$Command
+Invoke-Expression -Command $Command
+
+# In the claros.interfaces.python/dist directory will be the source tarball and the binary wheel file
+
+# Stop-Transcript
+Set-Location ..\
