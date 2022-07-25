f8e17ece36246576b40a9119a1c9e18b99a76405
Dan <daniel.edwards@aquaticinformatics.com>
2022-07-25 08:31:29 -0600

    version, relative import script for packaging

--- src/gen_python.ps1
+++ src/gen_python.ps1
@@ -3,22 +3,35 @@ mkdir -Force ./one_interfaces_python
 
 Copy-Item ./python/* ./one_interfaces_python
 mkdir -Force ././one_interfaces_python/one_interfaces
-Copy-Item ./one.interfaces.protocolbuffers/proto/flat/out/python/*.* ./one_interfaces_python/one_interfaces
+Copy-Item ../../one.interfaces.protocolbuffers/proto/flat/out/python/*.* ./one_interfaces_python/one_interfaces
 Move-Item -Force ./one_interfaces_python/__init__.py ./one_interfaces_python/one_interfaces
-Copy-Item -Force ./one_interfaces_python/one_interfaces.pth ./one_interfaces_python/one_interfaces
+Copy-Item -Force ./one_interfaces_python/one_interfaces.pth ./one_interfaces_python/
 
 Set-Location ./one_interfaces_python
 
 # Make a source package
-$Command = "python setup.py clean build sdist"
+$Command = "python setup.py clean build"
 Write-Output `n$Command
 Invoke-Expression -Command $Command
+Set-Location ./build/lib/one_interfaces
+$Command = "python converter.py"
+Invoke-Expression -Command $Command
+Move-Item converter.py ../..
+Set-Location ../../../
 
+$Command = "python setup.py sdist"
+Invoke-Expression -Command $Command
 # Make a binary package
-$Command = "python setup.py clean build bdist_wheel"
+$Command = "python setup.py clean build"
 Write-Output `n$Command
 Invoke-Expression -Command $Command
-
+Set-Location ./build/lib/one_interfaces
+$Command = "python converter.py"
+Invoke-Expression -Command $Command
+Move-Item converter.py ../..
+Set-Location ../../../
+$Command = "python setup.py bdist_wheel"
+Invoke-Expression -Command $Command
 # In the one.interfaces.python/dist directory will be the source tarball and the binary wheel file
 
 # Stop-Transcript
--- src/python/README.md
+++ src/python/README.md
@@ -1,6 +1,6 @@
-Claros_Interfaces
-======
-Python bindings for Claros protobuf packages.
+One_Interfaces
+==============
+Python bindings for One protobuf packages.
 
 Supported File Formats
 ======================
@@ -12,9 +12,9 @@ protobuf runtime
 
 Usage
 =====
-pip install claros_interfaces-<version>-py2.py3-none-any.whl
+pip install one_interfaces-<version>-py2.py3-none-any.whl
 Add the module to the sys.path by either adding the module path to PYTHONPATH 
-or copy claros_interfaces.pth to the python site-packages directory.
+or copy one_interfaces.pth to the python site-packages directory.
 
 Testing
 =======
