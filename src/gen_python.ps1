72dbd433e4e9573a8c868c092f1acc858941194c
Dan <daniel.edwards@aquaticinformatics.com>
2024-03-29 10:17:55 -0500

    updates

--- src/gen_python.ps1
+++ src/gen_python.ps1
@@ -3,18 +3,21 @@ mkdir -Force ./one_interfaces_python
 
 Copy-Item ./python/* ./one_interfaces_python
 mkdir -Force ././one_interfaces_python/one_interfaces
-Copy-Item ./one.interfaces.protocolbuffers/proto/flat/out/python/*.* ./one_interfaces_python/one_interfaces
+Copy-Item ./../../one.interfaces.protocolbuffers/proto/flat/out/python/*.* ./one_interfaces_python/one_interfaces
 Move-Item -Force ./one_interfaces_python/__init__.py ./one_interfaces_python/one_interfaces
 Copy-Item -Force ./one_interfaces_python/one_interfaces.pth ./one_interfaces_python/
 
 Set-Location ./one_interfaces_python
 
+
 # Make a source package
 $Command = "python setup.py clean build"
 Write-Output `n$Command
 Invoke-Expression -Command $Command
+Copy-Item -Force ./converter.py ./build/lib/one_interfaces
 Set-Location ./build/lib/one_interfaces
 $Command = "python converter.py"
+Write-Output `n$Command
 Invoke-Expression -Command $Command
 Move-Item converter.py ../..
 Set-Location ../../../
